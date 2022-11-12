97c51a7aeb8c511a22b9a15f1cc010c727945ae2
Hsiang Nianian <Hsiangnianian@outlook.com>
2022-11-12 10:30:26 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,6 @@
-import { defineUserConfig } from 'vuepress'
-
-export default defineUserConfig({
-  lang: 'zh-CN',
-  title: 'c2xj3xp5x-pages',
-  description: 'c2xj3xp5x-pages',
-})
+module.exports = {
+  title: 'c2xj3xp5x-pages',  // 设置网站标题
+  dest: './dist',    // 设置输出目录
+  base: '/pages/', // 设置站点根路径
+  repo: 'https://github.com/CJPP-C2xJ3xP5x/pages' // 添加 github 链接
+}
